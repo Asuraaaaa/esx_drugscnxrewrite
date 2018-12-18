5fd75aae34cdb82c6d2e8c9b3f28f35434d2300e
lownignitus <psyvampignitus@gmail.com>
2018-12-17 23:48:22 -0700

    Update esx_drugs.sql
    changed DB to all English

--- esx_drugs.sql
+++ esx_drugs.sql
@@ -1,10 +1,10 @@
 INSERT INTO `items` (name, label, `limit`) VALUES
 	('weed', 'Weed', 50),
-	('weed_pouch', 'Pochon de weed', 100),
+	('weed_pouch', 'Pouch of weed', 100),
 	('coke', 'Coke', 50),
-	('coke_pouch', 'Pochon de coke', 100),
+	('coke_pouch', 'Pouch of coke', 100),
 	('meth', 'Meth', 50),
-	('meth_pouch', 'Pochon de meth', 100),
+	('meth_pouch', 'Pouch of meth', 100),
 	('opium', 'Opium', 50),
-	('opium_pouch', 'Pochon de opium', 100)
+	('opium_pouch', 'Pouch of opium', 100)
 ;
