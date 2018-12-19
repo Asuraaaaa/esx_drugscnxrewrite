9a5b2aee2cffe86e1827c8554860b2e841f710bb
lownignitus <psyvampignitus@gmail.com>
2018-12-18 22:28:24 -0700

    Changed default variables.
    Changed baggy limit
    changed pouch to baggy

--- esx_drugs.sql
+++ esx_drugs.sql
@@ -1,10 +1,10 @@
 INSERT INTO `items` (name, label, `limit`) VALUES
-	('weed', 'Weed', 50),
+	('weed', 'Weed', 20),
 	('weed_pouch', 'Pouch of weed', 100),
-	('coke', 'Coke', 50),
+	('coke', 'Coke', 20),
 	('coke_pouch', 'Pouch of coke', 100),
-	('meth', 'Meth', 50),
+	('meth', 'Meth', 20),
 	('meth_pouch', 'Pouch of meth', 100),
-	('opium', 'Opium', 50),
+	('opium', 'Opium', 20),
 	('opium_pouch', 'Pouch of opium', 100)
 ;
